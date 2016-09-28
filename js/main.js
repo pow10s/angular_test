1be6b0729fd2bcaad88f131481dbd67f9e874f6c
Dima Stos <Dima Stos>
2016-09-29 01:21:57 +0300

    created json and read data from json

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var app = angular.module('tabs', []);
+app.controller('tabsData', function($scope, $http) {
+  $http.get("tab1.php").then(function (response) {
+      $scope.myData = response.data.records;
+  });
+});
---
+++ tab1.php
@@ -0,0 +1,32 @@
+{
+"records":[
+	{
+	"name":"Add new column from conditional logic in Octave",
+	"link":"http://stackoverflow.com/questions/39757860/add-new-column-from-conditional-logic-in-octave",
+	"votes":"5",
+	"answers": "3",
+	"view": "4",
+	"userName" : "Dima",
+	"time" : "15 sec"
+	},
+	{
+	"name":"Add new column from conditional logic in Octave",
+	"link":"http://stackoverflow.com/questions/39757860/add-new-column-from-conditional-logic-in-octave",
+	"votes":"5",
+	"answers": "3",
+	"view": "4",
+	"userName" : "Dima",
+	"time" : "15 sec"
+	},
+	{
+	"name":"Add new column from conditional logic in Octave",
+	"link":"http://stackoverflow.com/questions/39757860/add-new-column-from-conditional-logic-in-octave",
+	"votes":"5",
+	"answers": "3",
+	"view": "4",
+	"userName" : "Dima",
+	"time" : "15 sec"
+	}
+
+]
+}
