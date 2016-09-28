822ae913bb87bdad5169cfa2a2eeadf86179f088
Dima Stos <Dima Stos>
2016-09-29 02:28:13 +0300

    Added featured tab

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ app.controller('tabsData', function($scope, $http) {
   $http.get("tab1.php").then(function (response) {
       $scope.myData = response.data.records;
   });
+
+  $http.get("tab2.php").then(function (response) {
+      $scope.myData2 = response.data.records;
+  });
 });
+
---
+++ tab2.php
@@ -0,0 +1,42 @@
+{
+"records":[
+	{
+	"name":"Featured",
+	"link":"#featured",
+	"votes":"3",
+	"answers": "2",
+	"view": "2",
+	"userName" : "Oleg",
+	"time" : "45 sec",
+	"tags":[
+		{
+		"tagName":"feat1",
+		"tagLink":"#feat1"
+		},
+		{
+		"tagName":"feat2",
+		"tagLink":"#feat2"
+		}	
+	]
+	},
+			{
+	"name":"Featured2",
+	"link":"#featured2",
+	"votes":"32",
+	"answers": "22",
+	"view": "22",
+	"userName" : "Ivan",
+	"time" : "1 min",
+	"tags":[
+		{
+		"tagName":"feat3",
+		"tagLink":"#feat3"
+		},
+		{
+		"tagName":"feat3",
+		"tagLink":"#feat3"
+		}	
+	]
+	}
+]
+}
