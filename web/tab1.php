1dcb3600378becf206ee03286626ce8da31cbf32
stosdima <stosdima@gmail.com>
2016-10-01 11:33:14 +0300

    Installed silex framework

---
+++ web/tab1.php
@@ -0,0 +1,42 @@
+{
+"records":[
+	{
+	"name":"Add new column from conditional logic in Octave",
+	"link":"http://stackoverflow.com/questions/39757860/add-new-column-from-conditional-logic-in-octave",
+	"votes":"5",
+	"answers": "3",
+	"view": "4",
+	"userName" : "Dima",
+	"time" : "15 sec",
+	"tags":[
+		{
+		"tagName":"test1",
+		"tagLink":"#link1"
+		},
+		{
+		"tagName":"test2",
+		"tagLink":"#link2"
+		}	
+	]
+	},
+		{
+	"name":"Xcode Unit Testing with Cocoapods",
+	"link":"http://stackoverflow.com/questions/38216090/xcode-unit-testing-with-cocoapods",
+	"votes":"1",
+	"answers": "1",
+	"view": "0",
+	"userName" : "Petro",
+	"time" : "1 hour",
+	"tags":[
+		{
+		"tagName":"node.js",
+		"tagLink":"#link1"
+		},
+		{
+		"tagName":"mongoose-schema",
+		"tagLink":"#link2"
+		}	
+	]
+	}
+]
+}
